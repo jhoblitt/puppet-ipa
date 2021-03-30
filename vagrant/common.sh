eea16e371d961956cf0e4f3032c2fcb254225bbb
Samuli Seppänen <samuli@openvpn.net>
2021-03-30 10:09:51 +0300

    Vagrant: install puppetlabs-firewall
    While firewall resources are not used by default, it is useful to have that
    capability for custom tests in Vagrant.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vagrant/common.sh
+++ vagrant/common.sh
@@ -5,6 +5,7 @@ puppet module install puppetlabs-stdlib
 puppet module install crayfishx-firewalld
 puppet module install puppet-selinux
 puppet module install saz-resolv_conf
+puppet module install puppetlabs-firewall
 if [ -d /tmp/modules/easy_ipa ]; then rm -rf /tmp/modules/easy_ipa; fi
 mkdir -p /tmp/modules/easy_ipa
 cp -r /vagrant/* /tmp/modules/easy_ipa
